6a794457e8318cd1bddf6fd3ec52b8e5a2e56de0
Timothy Li <timothy2005@163.com>
2020-02-20 22:25:00 +0800

    Update pageService.js
    pageSize changed to 50

--- _CUSTOMIZE_/ext-smartedit/cmssmartedit/web/features/cmssmarteditContainer/services/pages/pageService.js
+++ _CUSTOMIZE_/ext-smartedit/cmssmartedit/web/features/cmssmarteditContainer/services/pages/pageService.js
@@ -65,7 +65,7 @@ angular.module('pageServiceModule', [
                 typeCode: pageTypeCode,
                 itemSearchParams: "defaultPage:true,pageStatus:ACTIVE",
                 currentPage: 0,
-                pageSize: 666
+                pageSize: 50
             });
             return cmsitemsRestService.get(extendedParams).then(function(primaryPages) {
                 return primaryPages.response;
